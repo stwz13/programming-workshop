3523d2bfbbba16f01e4c7d54ec6006f558dc1d91
Софья Передерина <sonya.perederina@mail.ru>
2025-03-10 20:33:42 +0600

    refactor: adding a typedef

--- task3/main.c
+++ task3/main.c
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 void test1() {
-  struct Stack *new_stack = create_stack(3);
+  struct Stack *new_stack = create_stack(2);
   push(new_stack, 5);
   push(new_stack, 7);
+  push(new_stack, 9);
   assert(is_empty(new_stack) == 0 && pop(new_stack) == 7);
   assert(is_empty(new_stack) == 0 && pop(new_stack) == 5);
+  assert(is_empty(new_stack) == 1);
   delete_stack(new_stack);
 }
 int main() {
--- task3/task3.c
+++ task3/task3.c
@@ -7,7 +7,7 @@ typedef struct Stack {
   int number_of_elements;
 } Stack;
 Stack *create_stack(int size_of_stack) {
-  struct Stack *stack = malloc(sizeof(struct Stack));
+  struct Stack *stack = malloc(sizeof(Stack));
   stack->elements = malloc(size_of_stack * sizeof(int));
   stack->number_of_elements = 0;
   stack->max_size = size_of_stack;
@@ -19,21 +19,15 @@ void expand_stack(Stack *stack) {
 }
 void push(Stack *stack, int new_element) {
   if (stack->max_size < stack->number_of_elements + 1) {
-    printf("Stack is full.Memory is being added.");
+    printf("Stack is full. Memory is being added.\n");
     expand_stack(stack);
   } else {
     stack->elements[stack->number_of_elements] = new_element;
     stack->number_of_elements++;
   }
 }
-
-int is_empty(struct Stack *stack) {
-  if (stack->number_of_elements == 0) {
-    return 1;
-  } else
-    return 0;
-}
-int pop(struct Stack *stack) {
+int is_empty(Stack *stack) { return (stack->number_of_elements == 0); }
+int pop(Stack *stack) {
   if (is_empty(stack) == 1) {
     printf("Stack is empty\n");
     exit(EXIT_FAILURE);
@@ -43,7 +37,7 @@ int pop(struct Stack *stack) {
     return last_element;
   }
 }
-void delete_stack(struct Stack *stack) {
+void delete_stack(Stack *stack) {
   free(stack->elements);
   free(stack);
 }
