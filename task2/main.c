16a358f6072862920bd4a3d7e537e385f23f9e84
Elena <ulidka12345@yandex.ru>
2025-03-12 21:23:12 +0600

    fix error

--- task2/main.c
+++ task2/main.c
@@ -5,18 +5,15 @@
 double f1(double x) { return x; }
 double f2(double x) { return x * x; };
 double f3(double x) { return sqrt(x); }
-double f4(double x) { return sin(x); }
-double f5(double x) { return 1 / x; }
-void test1() { assert(fabs(integration(f1, -1, 1, 10) - 0) < 1E-7); }
-void test2() { assert(fabs(integration(f2, 0, 1, 100000) - 0.333) < 1E-7); }
-void test3() { assert(fabs(integration(f3, 1, 5, 100) - 6.78) < 1E-7); }
-void test4() { assert(fabs(integration(f4, 1, 3.14 / 2, 1000) - 1) < 1E-7); }
-void test5() { assert(fabs(integration(f5, 5, 9, 1000000) - 0.58) < 1E-7); }
+double f4(double x) { return 1 / x; }
+void test1() { assert(fabs(integration(f1, -1, 1, 10000) - 0) < 1E-5); }
+void test2() { assert(fabs(integration(f2, 0, 1, 1000000) - 0.333333) < 1E-6); }
+void test3() { assert(fabs(integration(f3, 1, 5, 10000) - 6.7868) < 1E-4); }
+void test4() { assert(fabs(integration(f4, 5, 9, 100000) - 0.58778) < 1E-5); }
 int main() {
   test1();
   test2();
   test3();
   test4();
-  test5();
   return 0;
 }
