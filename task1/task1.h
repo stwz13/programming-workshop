d9bfee81aa18c25700ba3f444e53299a2cedbb00
sabinamuk <stm1573@icloud.com>
2025-03-12 20:37:02 +0600

    Create task1.h

---
+++ task1/task1.h
@@ -0,0 +1,4 @@
+int sign(double a);
+double tolerance(double a, double eps);
+double *solution_of__quadratic_equation(double a, double b, double c,
+                                        double eps, double disriminant_eps);
