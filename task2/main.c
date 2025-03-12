8e4bc848df4da9cfe3275954f9e3e5e3539ab76a
Elena <ulidka12345@yandex.ru>
2025-03-12 20:36:26 +0600

    fix error

--- task2/main.c
+++ task2/main.c
@@ -6,11 +6,11 @@ double f2(double x) { return x * x; };
 double f3(double x) { return sqrt(x); }
 double f4(double x) { return sin(x); }
 double f5(double x) { return 1 / x; }
-void test1() { assert(fabs(integration(f1, -1, 1, 10) - 0) < 10E-7); }
-void test2() { assert(fabs(integration(f2, 0, 1, 100000) - 0.333) < 10E-7); }
-void test3() { assert(fabs(integration(f3, 1, 5, 100) - 6.78) < 10E-7); }
-void test4() { assert(fabs(integration(f4, 1, 3.14 / 2, 1000) - 1) < 10E-7); }
-void test5() { assert(fabs(integration(f5, 5, 9, 1000000) - 0.58) < 10E-7); }
+void test1() { assert(fabs(integration(f1, -1, 1, 10) - 0) < 1E-7); }
+void test2() { assert(fabs(integration(f2, 0, 1, 100000) - 0.333) < 1E-7); }
+void test3() { assert(fabs(integration(f3, 1, 5, 100) - 6.78) < 1E-7); }
+void test4() { assert(fabs(integration(f4, 1, 3.14 / 2, 1000) - 1) < 1E-7); }
+void test5() { assert(fabs(integration(f5, 5, 9, 1000000) - 0.58) < 1E-7); }
 int main() {
   test1();
   test2();
