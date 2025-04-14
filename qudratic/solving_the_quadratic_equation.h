aa520f8f88e55345575cf6779eceb88b55ca2023
sabinamuk <stm1573@icloud.com>
2025-04-14 20:16:48 +0600

    Update solving_the_quadratic_equation.h

--- qudratic/solving_the_quadratic_equation.h
+++ qudratic/solving_the_quadratic_equation.h
@@ -1,2 +1,2 @@
-double *solutions_of_quadratic_equation(double a, double b, double c,
-                                        double eps, int *completion);
+int solutions_of_quadratic_equation(double a, double b, double c, double eps,
+                                    double **solutions);
