c443266dbb3ee4fa2124427d7e63e193f02c55c6
sabinamuk <stm1573@icloud.com>
2025-04-14 20:16:50 +0600

    Update solving_the_quadratic_equation.c

--- qudratic/solving_the_quadratic_equation.c
+++ qudratic/solving_the_quadratic_equation.c
@@ -1,54 +1,55 @@
 
 #include <math.h>
 #include <stdlib.h>
+
 #define IS_NOT_QUADRATIC -1;
 #define DISCRIMINANT_IS_NEGATIVE -2;
+#define ONE_ROOT 1
+#define TWO_ROOTS 2
 
-double *solutions_of_quadratic_equation(double a, double b, double c,
-                                        double eps, int *completion) {
+int solutions_of_quadratic_equation(double a, double b, double c, double eps,
+                                    double **solutions) {
 
-  double *solutions = NULL;
+  *solutions = NULL;
 
   if (fabs(a) < eps) {
-    *completion = IS_NOT_QUADRATIC;
+    return IS_NOT_QUADRATIC;
   } else {
-
     b = b / a;
-    int sign_b = b >= 0 ? 1 : -1;
-
     c = c / a;
+
     double discriminant = b * b - 4 * c;
 
     if (fabs(discriminant) < eps) {
 
-      *completion = 1;
-      solutions = malloc(sizeof(double));
+      *solutions = malloc(sizeof(double));
+      (*solutions)[0] = -b / 2;
 
-      solutions[0] = -b / 2;
+      return ONE_ROOT;
     }
 
     else if (discriminant < -eps) {
-      *completion = DISCRIMINANT_IS_NEGATIVE;
+      return DISCRIMINANT_IS_NEGATIVE;
     }
 
     else {
-      solutions = malloc(2 * sizeof(double));
-      *completion = 2;
+      *solutions = malloc(2 * sizeof(double));
+      int sign_b = b >= 0 ? 1 : -1;
 
       double x1 = -(b + sign_b * sqrt(discriminant)) / 2;
       double x2 = c / x1;
 
       if (x2 > x1) {
-        solutions[0] = x1;
-        solutions[1] = x2;
+        (*solutions)[0] = x1;
+        (*solutions)[1] = x2;
       }
 
       else {
-        solutions[0] = x2;
-        solutions[1] = x1;
+        (*solutions)[0] = x2;
+        (*solutions)[1] = x1;
       }
+
+      return TWO_ROOTS;
     }
   }
-
-  return solutions;
 }
