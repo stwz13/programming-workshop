e9f2eed79e2bddf8d2af47f8b2266d4e1def66a7
Elena <ulidka12345@yandex.ru>
2025-04-17 16:46:02 +0600

    fix file

--- c20/main.c
+++ c20/main.c
@@ -9,23 +9,28 @@ double funct4(double x) { return sqrt(x); }
 double funct5(double x) { return sin(x); }
 
 void test_linear_function() { assert(fabs(integration(funct1, 5, 15, 100000) - 100) < 1E-2); }
-void test_quadratic_function() {
+void test_quadratic_function()
+{
   assert(fabs(integration(funct2, 7, 35, 100000000) - 14177.33333) < 1E-3);
 }
 
-void test_inverse_function() {
+void test_inverse_function()
+{
   (assert(fabs(integration(funct3, 5, 9, 100000) - 0.587) < 1E-3));
 }
 
-void test_sqrt_function() {
+void test_sqrt_function()
+{
   (assert(fabs(integration(funct4, 3, 5, 100000000) - 3.98) < 1E-2));
 }
 
-void test_sine_function() {
+void test_sine_function()
+{
   (assert(fabs(integration(funct5, 1, 3, 100000) - 1.53) < 1E-3));
 }
 
-int main() {
+int main()
+{
   test_linear_function();
   test_quadratic_function();
   test_inverse_function();
