e155b9c9c04f3153982510d28168883920cb159b
Elena <ulidka12345@yandex.ru>
2025-04-17 17:33:55 +0600

    Revert "change names of tests"
    This reverts commit d17a7238316179460910b20272d22132a838b3c4.

--- c20/main.c
+++ c20/main.c
@@ -8,28 +8,28 @@ double funct3(double x) { return 1 / x; }
 double funct4(double x) { return sqrt(x); }
 double funct5(double x) { return sin(x); }
 
-void test_linear_function() { assert(fabs(integration(funct1, 5, 15, 100000) - 100) < 1E-2); }
-void test_quadratic_function() {
+void test1() { assert(fabs(integration(funct1, 5, 15, 100000) - 100) < 1E-2); }
+void test2() {
   assert(fabs(integration(funct2, 7, 35, 100000000) - 14177.33333) < 1E-3);
 }
 
-void test_inverse_function() {
+void test3() {
   (assert(fabs(integration(funct3, 5, 9, 100000) - 0.587) < 1E-3));
 }
 
-void test_sqrt_function() {
+void test4() {
   (assert(fabs(integration(funct4, 3, 5, 100000000) - 3.98) < 1E-2));
 }
 
-void test_sine_function() {
+void test5() {
   (assert(fabs(integration(funct5, 1, 3, 100000) - 1.53) < 1E-3));
 }
 
 int main() {
-  test_linear_function();
-  test_quadratic_function();
-  test_inverse_function();
-  test_sqrt_function();
-  test_sine_function();
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
   return 0;
 }
