69646e1320f3f06c031f7fa21ff9514d83f2a2df
stwz13 <sonya.perederina@mail.ru>
2025-05-22 11:41:16 +0600

    init: gc_counter

---
+++ gc/gc.h
@@ -0,0 +1,14 @@
+#include "../pool_allocator/pool_allocator.h"
+#include <stdlib.h>
+
+#define ALLOCATION_ERROR -1
+typedef struct {
+  size_t count;
+  void *object;
+  pool_allocator *allocator;
+} ref_count_t;
+
+int ref_create(ref_count_t *ref_object, pool_allocator *allocator,
+               void *object);
+int ref_increase(ref_count_t *ref);
+int ref_reduce(ref_count_t *ref);
