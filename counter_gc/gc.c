adcf1de7b99f84346f867d068bb001676616511c
stwz13 <sonya.perederina@mail.ru>
2025-05-22 12:14:23 +0600

    garbage_collector_counter

---
+++ counter_gc/gc.c
@@ -0,0 +1,36 @@
+#include "gc.h"
+
+int ref_create(ref_count_t *ref_object, pool_allocator *allocator,
+               void *object) {
+  if (!allocator || !object || !ref_object)
+    return ALLOCATION_ERROR;
+
+  ref_object->allocator = allocator;
+  ref_object->object = object;
+  ref_object->count = 1;
+
+  return SUCCESSFUL_COMPLETION;
+}
+
+int ref_increase(ref_count_t *ref) {
+  if (!ref)
+    return ALLOCATION_ERROR;
+  ref->count++;
+  return SUCCESSFUL_COMPLETION;
+}
+
+int ref_reduce(ref_count_t *ref) {
+  if (!ref)
+    return ALLOCATION_ERROR;
+
+  ref->count--;
+  if (ref->count == 0) {
+
+    if (ref->object == NULL)
+      return ALLOCATION_ERROR;
+
+    pool_free(ref->allocator, ref->object);
+    pool_free(ref->allocator, ref);
+  }
+  return SUCCESSFUL_COMPLETION;
+}
